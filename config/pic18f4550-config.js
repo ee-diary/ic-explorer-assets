bedcb4d9e66ada283e5dd9256c116c1d46193d7f
ee-diary <91747305+ee-diary@users.noreply.github.com>
2026-03-29 06:13:08 +0545

    Create pic18f4550-config.js

---
+++ config/pic18f4550-config.js
@@ -0,0 +1,16 @@
+// configs/pic18f4550-config.js
+window.IC_CONFIG = {
+  partName: 'PIC18F4550',
+  partMPN: 'PIC18F4550-I/P',
+  manufacturer: 'MICROCHIP',
+  package: 'DIP-40',
+  pinCount: 40,
+  
+  // ... rest of config (copy from your file)
+  
+  pins: [ /* ... */ ],
+  altFuncs: { /* ... */ },
+  quickSpecs: [ /* ... */ ],
+  dsSpecs: [ /* ... */ ],
+  dsFeatures: [ /* ... */ ],
+};
