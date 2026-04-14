c8d7b926402b0cc1af531232d6b5d352c503497f
ee-diary <91747305+ee-diary@users.noreply.github.com>
2026-04-14 15:23:06 +0545

    Update arduino-uno-renderer.js

--- renderers/arduino-uno-renderer.js
+++ renderers/arduino-uno-renderer.js
@@ -192,10 +192,10 @@
     app(g, mkt('USB-SERIAL', { fill: '#242424', 'font-family': 'monospace', 'font-size': '6', 'text-anchor': 'middle', x: '284', y: '181' }));
 
     // Crystal Oscillator 16MHz - MOVED 10px UP and 45px LEFT (from x:260,y:165 to x:215,y:155)
-    app(g, mk('rect',   { x: '192', y: '155', width: '26', height: '42', rx: '9', fill: 'url(#unoSilvGr)', stroke: '#888', 'stroke-width': '1.5' }));
+    app(g, mk('rect',   { x: '192', y: '155', width: '36', height: '52', rx: '9', fill: 'url(#unoSilvGr)', stroke: '#888', 'stroke-width': '1.5' }));
     app(g, mk('rect',   { x: '196', y: '163', width: '18', height: '28', rx: '5', fill: '#c0c0c0' }));
-    app(g, mkt('16',    { fill: '#555', 'font-family': 'monospace', 'font-size': '6', 'text-anchor': 'middle', x: '201', y: '175' }));
-    app(g, mkt('MHz',   { fill: '#555', 'font-family': 'monospace', 'font-size': '5', 'text-anchor': 'middle', x: '201', y: '183' }));
+    app(g, mkt('16',    { fill: '#555', 'font-family': 'monospace', 'font-size': '6', 'text-anchor': 'middle', x: '203', y: '175' }));
+    app(g, mkt('MHz',   { fill: '#555', 'font-family': 'monospace', 'font-size': '5', 'text-anchor': 'middle', x: '203', y: '183' }));
 
     // ATmega328P — DIP-28
     (function () {
