8b5dd5d32597db31b1f0ecc596b472e1599ed735
ee-diary <91747305+ee-diary@users.noreply.github.com>
2026-04-15 00:22:06 +0545

    a

--- renderers/arduino-uno-renderer.js
+++ renderers/arduino-uno-renderer.js
@@ -199,7 +199,7 @@
 
     // ATmega328P — DIP-28
     (function () {
-      var by = 225;
+      var by = 225 - 40;
       var bh = 417 - 225 + 40; // Increase height (add 40px)
       var bw = 64;
       var bx = 86;
